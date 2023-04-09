9c87e71c87ec92bfa0b730f8e32e2e98f3581152
Rawier <74272440+Rawierdt@users.noreply.github.com>
2023-04-09 15:07:52 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Norma ISO 27001
+
+### Simple page where the content of the 27001 standard is summarized in Spanish.
+
+![IMG](./src/preview.png)
---
+++ index.js
@@ -0,0 +1,35 @@
+$("#whatIsButton").click(function(){
+   $(".whatIs").slideToggle();
+});
+
+$("#contenido1Button").click(function(){
+   $(".contenido1").slideToggle();
+});
+
+$("#contenido2Button").click(function(){
+   $(".contenido2").slideToggle();
+});
+
+$("#contenido3Button").click(function(){
+   $(".contenido3").slideToggle();
+});
+
+$("#contenido4Button").click(function(){
+   $(".contenido4").slideToggle();
+});
+
+$("#contenido5Button").click(function(){
+   $(".contenido5").slideToggle();
+});
+
+$("#contenido6Button").click(function(){
+   $(".contenido6").slideToggle();
+});
+
+$("#contenido7Button").click(function(){
+   $(".contenido7").slideToggle();
+});
+
+$("#contenido8Button").click(function(){
+   $(".contenido8").slideToggle();
+});
